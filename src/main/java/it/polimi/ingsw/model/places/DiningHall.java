0e374d7a0fee1dad7e524719a5833525c01313f0
EdoardoVolpentesta <100208972+EdoardoVolpentesta@users.noreply.github.com>
2022-03-20 13:00:36 +0100

    Update DiningHall.java

--- src/main/java/it/polimi/ingsw/model/places/DiningHall.java
+++ src/main/java/it/polimi/ingsw/model/places/DiningHall.java
@@ -3,19 +3,34 @@ package it.polimi.ingsw.model.places;
 import it.polimi.ingsw.model.entities.Student;
 import it.polimi.ingsw.model.utils.Color;
 
+import java.util.List;
+
 public class DiningHall implements StudentPlace {
-    @Override
-    public void addStudent(Student student) {
-        throw new UnsupportedOperationException();
-    }
+    private static List<Student> DiningStudents;
 
     @Override
-    public boolean getStudent(Color color) {
-        throw new UnsupportedOperationException();
+    public static void addStudent(Student student) {
+        Color col = Student.getColor(student);
+        if(DiningHall.getStudent(col) < 10){
+            DiningStudents.add(student);
+        }
+        else{throw new UnsupportedOperationException();}
+    }
+
+   @Override
+    public static int getStudent(Color color) {
+        int count = 0;
+        for(;;){
+            if(Student.color == col) count++;
+        }
+        return count;
     }
 
     @Override
     public Student getRandomStudent() {
-        throw new UnsupportedOperationException();
+        int index = (int)(Math.random() * DiningStudents.size());
+        Student student = DiningStudents.get(index);
+        DiningStudents.remove(student);
+        return student;
     }
 }
