8c76b6059a331a714fd785fdac8e0de824067b25
EdoardoVolpentesta <100208972+EdoardoVolpentesta@users.noreply.github.com>
2022-03-20 10:52:09 +0100

    Update School.java

--- src/main/java/it/polimi/ingsw/model/places/School.java
+++ src/main/java/it/polimi/ingsw/model/places/School.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.model.places;
 
+import it.polimi.ingsw.model.entities.Player;
 import it.polimi.ingsw.model.entities.Student;
+import it.polimi.ingsw.model.entities.Tower;
 import it.polimi.ingsw.model.utils.Color;
 
 public class School {
@@ -8,11 +10,37 @@ public class School {
     private DiningHall dining_hall;
     private TowerHall tower_hall;
 
+
     public void addStudent(Student student, StudentPlace place){
-        throw new UnsupportedOperationException();
+        if(place.equals("Dining")) {DiningHall.addStudent(student);}
+        else if(place.equals("Entrance")){Entrance.addStudent(student);}
+        else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public static void TowerRemove(Tower tower){
+        TowerHall.remove(tower);
     }
 
     public int getNumberOfStudents(Color col){
-        throw new UnsupportedOperationException();
+        int count = 0;
+        int flag = 0;
+        for(;;){
+            if(Student.color.equals(col)) count++;
+        }
+        if(count == 3){
+            Player.addCoins(1);
+            flag = 1;
+        }
+        if(count == 6 && flag==1){
+            Player.addCoins(1);
+            flag = 2;
+        }
+        if(count == 9 && flag == 2){
+            Player.addCoins(1);
+            flag = 3;
+        }
+        return count;
     }
 }
