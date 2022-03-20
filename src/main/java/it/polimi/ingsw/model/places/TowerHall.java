df67ec38b23fbb3be31d3bd7972dea4c6f0d0e5e
EdoardoVolpentesta <100208972+EdoardoVolpentesta@users.noreply.github.com>
2022-03-20 12:59:01 +0100

    Update TowerHall.java

--- src/main/java/it/polimi/ingsw/model/places/TowerHall.java
+++ src/main/java/it/polimi/ingsw/model/places/TowerHall.java
@@ -4,33 +4,70 @@ import it.polimi.ingsw.model.entities.Student;
 import it.polimi.ingsw.model.entities.Tower;
 import it.polimi.ingsw.model.utils.Color;
 
+import java.util.List;
+
 public class TowerHall implements StudentPlace, TowerPlace {
-    @Override
-    public void addStudent(Student student) {
-        throw new UnsupportedOperationException();
-    }
+    private static List<Tower> towers;
+    private final List<Student> students;
 
-    @Override
-    public boolean getStudent(Color color) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Student getRandomStudent() {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean moveTowerTo(int island_index){ //forse inutile
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addTower(Tower tower) {
-        throw new UnsupportedOperationException();
+    public TowerHall(List<Student> students) {
+        this.students = students;
     }
 
     @Override
     public boolean getTower(Color color) {
-        throw new UnsupportedOperationException();
+        int to_remove = -1;
+        for(int i = 0; i < towers.size() && to_remove==-1; i++){
+            if(towers.get(i).getColor().equals(color)){
+                to_remove = i;
+            }
+        }
+        towers.remove(to_remove);
+        return true;
     }
+
+
+
+    @Override
+    public boolean getStudent(Color color) {
+        int to_remove = -1;
+        for(int i = 0; i < students.size() && to_remove==-1; i++){
+            if(students.get(i).getColor().equals(color)){
+                to_remove = i;
+            }
+        }
+        students.remove(to_remove);
+        return true;
+    }
+
+    @Override
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    @Override
+    public boolean addTower(Tower tower) {
+        if(towers.size() < 8){
+            return towers.add(tower);
+        }
+        else{
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public static void remove(Tower tower){
+        if(towers.size() > 0){
+            towers.remove(tower);
+        }
+        else{throw new UnsupportedOperationException();}
+    }
+
+    @Override
+    public Student getRandomStudent() {
+        int index = (int)(Math.random() * students.size());
+        Student student = students.get(index);
+        students.remove(student);
+        return student;
+    }
+
 }
