19ecb55fb686ca04c16045360f61fa1f15480070
EdoardoVolpentesta <100208972+EdoardoVolpentesta@users.noreply.github.com>
2022-03-24 18:01:33 +0100

    Update DiningHall.java

--- src/main/java/it/polimi/ingsw/model/places/DiningHall.java
+++ src/main/java/it/polimi/ingsw/model/places/DiningHall.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model.places;
 
+import it.polimi.ingsw.model.entities.Player;
 import it.polimi.ingsw.model.entities.Student;
 import it.polimi.ingsw.model.utils.Color;
 
@@ -9,7 +10,19 @@ public class DiningHall extends StudentPlace {
     public DiningHall(){
         super(0);
     }
-
+    
+    public boolean addStudent(Student student, Player player){
+        if(students.stream().filter(stud -> stud.getColor().equals(student.getColor())).count() < STUDENTS_PER_COLOR) {
+            students.add(student);
+            if (students.stream().filter(stud -> stud.getColor().equals(student.getColor())).count() %3 == 0 && 
+                    students.stream().filter(stud -> stud.getColor().equals(student.getColor())).count() > 0 ) {
+               player.addCoins(1);
+            }
+            return true;
+        }
+        return false;
+    }
+    
     @Override
     public boolean addStudent(Student student) {
         if(students.stream().filter(stud -> stud.getColor().equals(student.getColor())).count() < STUDENTS_PER_COLOR) {
