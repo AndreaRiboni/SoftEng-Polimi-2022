de9c1e9bf56ff27c9d41eaa3cfe68dde12aa097d
EdoardoVolpentesta <100208972+EdoardoVolpentesta@users.noreply.github.com>
2022-04-01 20:13:31 +0200

    Update GameBoardTest.java

--- src/test/java/it/polimi/ingsw/GameBoardTest.java
+++ src/test/java/it/polimi/ingsw/GameBoardTest.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model.places;
 
 import it.polimi.ingsw.model.entities.Player;
+import it.polimi.ingsw.model.entities.Professor;
 import it.polimi.ingsw.model.entities.Student;
 import it.polimi.ingsw.model.utils.Color;
 import it.polimi.ingsw.model.utils.EriantysException;
@@ -18,25 +19,27 @@ public class GameBoardTest {
         gameBoard.initializeCharacterDeck();
         gameBoard.initalizePlayers();
 
+        Professor professor = gameBoard.getProfFromColor(Color.YELLOW);
+
         Player player = gameBoard.getPlayers()[0];
         Player player2 = gameBoard.getPlayers()[1];
 
         List<Student> students = player.getEntranceStudents();
         List<Student> students2 = player2.getEntranceStudents();
 
-     //   System.out.println(students);
-     //   System.out.println(students2);
+        System.out.println(students);
+        System.out.println(students2);
 
         for(int i = 0; i < students.size(); i++){
-            if(students.get(i).getColor() == Color.YELLOW){
-                player.moveStudentInDiningHall(students.get(i));
-            }
+            player.moveStudentInDiningHall(students.get(i));
         }
         
         for(int i = 0; i < students2.size(); i++){
-            if(students2.get(i).getColor() == Color.RED){
-                player2.moveStudentInDiningHall(students2.get(i));
-            }
+            player2.moveStudentInDiningHall(students2.get(i));
         }
+
+        gameBoard.checkProf();
+
+        System.out.println(professor.getPlayer());
     }
 }
