33012657e5e0472cf22d30962f889d66a9586ab3
DavidePoidomani <101124324+DavidePoidomani@users.noreply.github.com>
2022-04-04 22:50:11 +0200

    setTowerOn test updated

---
+++ src/test/java/it/polimi/ingsw/model/entities/PlayerTest.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.model.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void moveTowerInIsland() {
+        //school nof towers --
+        //island can receive the tower
+        //check return
+    }
+
+    @Test
+    void swapEntranceDining() {
+        //test: the two students have been swapped
+        //check the number of students in both places
+        //check that you get the correct exception
+    }
+
+    @Test
+    void getTowerBack() {
+        //opposite of moveTowerInIsland
+    }
+}
