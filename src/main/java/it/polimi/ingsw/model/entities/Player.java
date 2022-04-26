559d6049cbf14fcde7620153f5206798191d4723
AndreaRiboniPolimi <andrea.riboni@mail.polimi.it>
2022-04-26 22:15:33 +0200

    coin value fix

--- src/main/java/it/polimi/ingsw/model/entities/Player.java
+++ src/main/java/it/polimi/ingsw/model/entities/Player.java
@@ -27,7 +27,7 @@ public class Player {
 
     public Player(GameBoard gameboard, int ID, Color color, boolean three_players) throws EriantysException {
         turn_value = 0;
-        coins = 100000000;
+        coins = 1;
         this.gameboard = gameboard;
         this.color = color;
         this.ID = ID;
