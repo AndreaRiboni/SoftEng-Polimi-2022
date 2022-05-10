0e9e4832025ed1922d9f190c217a67ea05d07670
EdoardoVolpentesta <100208972+EdoardoVolpentesta@users.noreply.github.com>
2022-05-10 18:30:25 +0200

    Update ProvaFinale_IngSw_GC51.java

--- src/main/java/it/polimi/ingsw/ProvaFinale_IngSw_GC51.java
+++ src/main/java/it/polimi/ingsw/ProvaFinale_IngSw_GC51.java
@@ -14,7 +14,28 @@ public class ProvaFinale_IngSw_GC51 {
     public static void main(String[] args) throws IOException {
         PropertyConfigurator.configure("log4j.properties");
         log.info("Application has started");
-        MultiServerLauncher hub = new MultiServerLauncher();
-        hub.startServer();
+        
+        System.out.println("Hi! Welcome to Eryantis!\nWhat do you want to launch?");
+        System.out.println("0. SERVER\n1. CLIENT (CLI INTERFACE)\n2. CLIENT (GUI INTERFACE)");
+        System.out.print(">");
+
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        try{
+            input = scanner.nextInt();
+        } catch (InputMismatchException e){
+            System.err.println("Something went wrong");
+            System.exit(-1);
+        }
+        switch (input) {
+            case 0 ->{   MultiServerLauncher hub = new MultiServerLauncher();
+                hub.startServer();
+            }
+            case 1 -> ClientLauncher.main(null);
+            /*case 2 -> {System.out.println("You selected the GUI interface, have fun!\nStarting...");
+                   GUI.main(null);
+            }*/
+            default -> System.err.println("Oh oh, be careful. You have to choose between:\n0.server\n1.client (CLI INTERFACE)\n2.client (GUI INTERFACE)");
+        }
     }
 }
