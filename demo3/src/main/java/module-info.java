6a5202fea5c078c1bdf865853e746beb1fca6824
EdoardoVolpentesta <100208972+EdoardoVolpentesta@users.noreply.github.com>
2022-05-10 13:37:58 +0200

    Add files via upload
    Prova GUI JAVAFX

---
+++ demo3/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.demo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires java.desktop;
+
+    opens com.example.demo3 to javafx.fxml;
+    exports com.example.demo3;
+}
