54d08a82e42fa76032c3d053f340f3793391ccba
EdoardoVolpentesta <100208972+EdoardoVolpentesta@users.noreply.github.com>
2022-05-18 11:49:09 +0200

    Add files via upload
    Added some images. Fixed the window's icon and choicebox/nickname

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -3,18 +3,22 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/graphics/Copertina.jpg")));
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.setResizable(false);
@@ -24,8 +28,9 @@ public class HelloApplication extends Application {
 
     public void start_game() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gameGraphic.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1270, 800);
+        Scene scene = new Scene(fxmlLoader.load());
         Stage stage = new Stage();
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/graphics/Copertina.jpg")));
         stage.setTitle("Gameboard");
         stage.setScene(scene);
         stage.setResizable(false);
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
 
 
     opens com.example.demo to javafx.fxml;
